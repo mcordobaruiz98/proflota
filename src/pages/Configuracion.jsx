7eeccf7790576ba6a9526e24b8514e4d650edda1
Mario Cordoba <macr-cor28@hotmail.com>
2026-06-10 11:23:52 -0500

    fix subir peajes

--- src/pages/Configuracion.jsx
+++ src/pages/Configuracion.jsx
@@ -140,6 +140,7 @@ function Configuracion({mostrarToast}) {
       <button
   style={{width:"100%", padding:"12px", background:t.colors.blue, color:"#fff", border:"none", borderRadius:t.radius.md, fontSize:t.fonts.sizeSm, fontWeight:t.fonts.weightBold, cursor:"pointer", marginTop:"10px"}}
   onClick={async () => {
+    console.log("mostrarToast tipo:", typeof mostrarToast)
   try {
     await subirPeajes();
     mostrarToast("165 peajes subidos a Firestore", "exito");
