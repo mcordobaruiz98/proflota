263deda10c5d2d20fc90b3fa393d75bbe8c406bc
Mario Cordoba <macr-cor28@hotmail.com>
2026-06-09 23:39:07 -0500

    fix numeros en ejes de 10 a 5

--- src/pages/mantenimiento/Llantas.jsx
+++ src/pages/mantenimiento/Llantas.jsx
@@ -67,7 +67,7 @@ function DiagramaLlantas({ total, llantas, onSelect, llantaActiva }) {
                 strokeWidth="2"
               />
               <text
-                x={l.x > 100 ? l.x + 10 : l.x - 10}
+                x={l.x > 100 ? l.x + 5 : l.x - 5}
                 y={y + 4}
                 textAnchor={l.x > 100 ? "start" : "end"}
                 fontSize="9"
