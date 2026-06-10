1b34e25fd3469f42baf2d1cd754c8997949557f8
Mario Cordoba <macr-cor28@hotmail.com>
2026-06-09 22:58:30 -0500

    fix detallevehiculo

--- src/pages/DetalleVehiculo.jsx
+++ src/pages/DetalleVehiculo.jsx
@@ -71,6 +71,7 @@ const seccionesHV = [
 ];
 
 function DetalleVehiculo({ vehiculos, viajes = [], mantenimientos = [], configMant = [], onAgregarMant, onEliminarMant, onAgregarConfig, onEliminarConfig, mostrarToast }) {
+  console.log("configMant:", configMant);
   const navigate  = useNavigate();
   const { id }    = useParams();
   const [tabActivo, setTabActivo] = useState("info");
