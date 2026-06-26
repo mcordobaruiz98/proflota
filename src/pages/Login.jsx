1fbae2824395f236e7d497bd6e1689f19d41a1e9
Mario Cordoba <macr-cor28@hotmail.com>
2026-06-25 19:32:25 -0500

    cambio de logo Navira login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -61,8 +61,8 @@ function Login() {
       {/* HERO */}
       <div style={styles.hero}>
         <div style={styles.logoWrap}>
-          <img src="/logo-navira.png" alt="Navira" style={{ height: "80px", objectFit: "contain" }} />
-          <p style={styles.logoSub}>Controla tus fletes. Maximiza tus ganancias.</p>
+          <img src="/logo-naviraT.png" alt="Navira" style={{ height: "80px", objectFit: "contain" }} />
+          
         </div>
       </div>
 
